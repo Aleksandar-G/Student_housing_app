3fd04f185d3127b0998c7fe4136af7be3a99a058
Martin Markov <I425706@fhict.nl>
2019-12-09 17:44:37 +0100

    Added tabs

--- CompanyApp/Form1.Designer.cs
+++ CompanyApp/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Login = new System.Windows.Forms.TabPage();
+            this.AddTenant = new System.Windows.Forms.TabPage();
+            this.ShowComplaints = new System.Windows.Forms.TabPage();
+            this.RemoveTenant = new System.Windows.Forms.TabPage();
+            this.ShowTenantDetails = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Login);
+            this.tabControl1.Controls.Add(this.AddTenant);
+            this.tabControl1.Controls.Add(this.ShowComplaints);
+            this.tabControl1.Controls.Add(this.RemoveTenant);
+            this.tabControl1.Controls.Add(this.ShowTenantDetails);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(788, 438);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(4, 22);
+            this.Login.Name = "Login";
+            this.Login.Padding = new System.Windows.Forms.Padding(3);
+            this.Login.Size = new System.Drawing.Size(780, 412);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            // 
+            // AddTenant
+            // 
+            this.AddTenant.Location = new System.Drawing.Point(4, 22);
+            this.AddTenant.Name = "AddTenant";
+            this.AddTenant.Padding = new System.Windows.Forms.Padding(3);
+            this.AddTenant.Size = new System.Drawing.Size(780, 412);
+            this.AddTenant.TabIndex = 1;
+            this.AddTenant.Text = "Add Tenant";
+            this.AddTenant.UseVisualStyleBackColor = true;
+            // 
+            // ShowComplaints
+            // 
+            this.ShowComplaints.Location = new System.Drawing.Point(4, 22);
+            this.ShowComplaints.Name = "ShowComplaints";
+            this.ShowComplaints.Size = new System.Drawing.Size(780, 412);
+            this.ShowComplaints.TabIndex = 2;
+            this.ShowComplaints.Text = "Show Complaints";
+            this.ShowComplaints.UseVisualStyleBackColor = true;
+            // 
+            // RemoveTenant
+            // 
+            this.RemoveTenant.Location = new System.Drawing.Point(4, 22);
+            this.RemoveTenant.Name = "RemoveTenant";
+            this.RemoveTenant.Size = new System.Drawing.Size(780, 412);
+            this.RemoveTenant.TabIndex = 3;
+            this.RemoveTenant.Text = "Remove Tenant";
+            this.RemoveTenant.UseVisualStyleBackColor = true;
+            // 
+            // ShowTenantDetails
+            // 
+            this.ShowTenantDetails.Location = new System.Drawing.Point(4, 22);
+            this.ShowTenantDetails.Name = "ShowTenantDetails";
+            this.ShowTenantDetails.Size = new System.Drawing.Size(780, 412);
+            this.ShowTenantDetails.TabIndex = 4;
+            this.ShowTenantDetails.Text = "Show Tenant Details";
+            this.ShowTenantDetails.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Login;
+        private System.Windows.Forms.TabPage AddTenant;
+        private System.Windows.Forms.TabPage ShowComplaints;
+        private System.Windows.Forms.TabPage RemoveTenant;
+        private System.Windows.Forms.TabPage ShowTenantDetails;
     }
 }
 
